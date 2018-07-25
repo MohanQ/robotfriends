3bed20683821ccdfab75ab876a55693f94c178a9
Andras Molnar <Andras.Molnar@sapagroup.com>
2018-07-25 21:42:37 +0200

    Get data from web api instead of static robots array.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import robots from './robots';
 import SearchBox from './SearchBox';
-import './App.css' 
+import './App.css'
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
-    } F
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(users => this.setState({ robots: users }));
+    }
 
     onSearchChange = (event) => {
         this.setState({ searchfield: event.target.value })
@@ -21,6 +27,10 @@ class App extends Component {
             this.state.robots.filter(robot =>
                 robot.name.toLocaleLowerCase().includes(
                     this.state.searchfield.toLocaleLowerCase()));
+        if(this.state.robots.length === 0)
+        {
+            return <h1>Loading...</h1>
+        }
         return (
             <div className='tc'>
                 <h1 className='f1'>RoboFriends</h1>
