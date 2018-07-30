5f9995dbd7ad5bcb6bd9dda8b0dbba9a9f8db292
Andras Molnar <andras.molnar@sapagroup.com>
2018-07-30 16:57:33 +0200

    add redux

---
+++ src/reducers.js
@@ -0,0 +1,12 @@
+export { CHANGE_SEARCHFIELD } from './constants'
+
+const initialState = {
+    searchField: ''
+}
+
+export const searchRobots = (state = initialState, action = {}) => {
+    switch (action.type) {
+        case CHANGE_SEARCHFIELD:
+            return Object.assign({}, state, { searchField: action.payload });
+    }
+};
